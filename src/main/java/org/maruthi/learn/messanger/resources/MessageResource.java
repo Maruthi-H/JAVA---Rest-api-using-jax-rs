178530915b148be57461629ab24c3486b906b2b0
i322309 <maruthi.h@sap.com>
2017-03-24 22:36:41 +0530

    implement PUT and DELETE methods

--- src/main/java/org/maruthi/learn/messanger/resources/MessageResource.java
+++ src/main/java/org/maruthi/learn/messanger/resources/MessageResource.java
@@ -3,7 +3,11 @@ package org.maruthi.learn.messanger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,19 +17,39 @@ import org.maruthi.learn.messanger.service.MessageService;
 import org.maruthi.learn.messanger.model.Message;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
 public class MessageResource {
 	MessageService messageService = new MessageService();
+	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
 	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
 	public Message getMessage(@PathParam("messageId") Long messageId){
 		return messageService.getMessage(messageId);
+	}
+	
+	@POST
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/messageId")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message){
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/messageId")
+	public void deleteMessage(@PathParam("messageId") long messageId){
+		messageService.removeMessage(messageId);
 	}
 	
 	
